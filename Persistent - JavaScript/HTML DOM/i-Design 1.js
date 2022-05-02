d9d0071125f1d6bb996579f5de043aa51dfab6c9
chirag <66241836+chiragbagri31@users.noreply.github.com>
2022-05-02 20:05:46 +0530

    Create i-Design 1.js

---
+++ Persistent - JavaScript/HTML DOM/i-Design 1.js
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<script>
+			function apply()
+			{
+				document.getElementById("innerDiv").style.marginTop = "100px"; 
+				document.getElementById("outerDiv").style.border= "1px solid"; 
+			}
+
+		</script>
+	</head>
+	<body onload="apply()">
+		<div id="outerDiv">
+			<div id="innerDiv">
+				A wedding planner is a professional who assists with the design, planning and management of a client's wedding. Weddings are significant events in people's lives and as such, couples are often willing to spend considerable amount of money to ensure that their weddings are well-organized. Wedding planners are often used by couples who work long hours and have little spare time available for sourcing and managing wedding venues and wedding suppliers
+			</div>
+		</div>
+	</body>
+</html>
