ec7c6e9aa98797723521e31bd3ae9a4bc908f33f
chirag <66241836+chiragbagri31@users.noreply.github.com>
2022-05-18 11:39:14 +0530

    Create i-Design.js

---
+++ Persistent - React With Redux/JSX/i-Design.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <div id="header">
+            <h2>Hello World</h2>
+          </div>
+    </div>
+  );
+}
+
+export default App;
